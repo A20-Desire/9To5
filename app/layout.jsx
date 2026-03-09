368dc6baba138995f4f1fbdac037932777346674
Desire <abrahamdesire053@gmail.com>
2026-03-09 12:05:16 +0100

    Use next/font for global fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,18 @@
 import '../styles/globals.css';
 import AppProviders from '../components/AppProviders';
+import { Inter, Oswald } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap',
+});
+
+const oswald = Oswald({
+  subsets: ['latin'],
+  variable: '--font-oswald',
+  display: 'swap',
+});
 
 export const metadata = {
   title: '9TO5 Consultancy',
@@ -12,12 +25,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
-      </head>
-      <body className="bg-black text-white antialiased">
+      <body className={`${inter.variable} ${oswald.variable} bg-black text-white antialiased`}>
         <AppProviders>{children}</AppProviders>
       </body>
     </html>
